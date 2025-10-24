c1934b0d71d092d0c639b0dbfe14ada1d24a7bc1
MirazZim <miraz.zim.38@gmail.com>
2025-10-24 15:37:44 +0600

    Removed use Client from App Folder to make it server component so that it will work super faster note : never use use client in app folder import instead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,7 @@
-'use client'
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
+
 const Homepage = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/login');
-  }, [router]);
-
-  return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-    }}>
-      Redirecting...
-    </div>
-  )
+  redirect('/login');
 }
 
-export default Homepage
+export default Homepage;
